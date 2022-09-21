0c46eff02fe9da95c9d928d3ee3eb91c0b07b395
Kevin Gicheha <gichehakevin@gmail.com>
2022-09-21 16:42:34 -0700

    added signup component

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,28 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
+// import { useHistory } from "react-router-dom";
 
 const Login = () => {
+
+  //storing data from the form
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  //to display the errors
+//   const [errors, setErrors] = useState([]);
+
+  //gives you access to the history instance that you may use to navigate.
+//   const history = useHistory();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormData(formData);
+
+    //make post request on submit
+    //    fetch(`/login`,{
+    //     method: 'POST',
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //       "Accept": "application/json"
+    //     },
+    //     body: JSON.stringify(formData)
+    //   })
+    //   .then(res => {
+    //     if(res.ok){
+    //         res.json().then(user => {
+    //             updateUser(user)
+    //             history.push(`/users/${user.id}`)
+    //         })
+    //     }else {
+    //         res.json().then(json => setErrors(json.errors))
+    //     }
+    // })
+
+    //reset form
+    setFormData({
+      email: "",
+      password: "",
+    });
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
   return (
     <>
       <div id="login">
-        <div class="logosection">
+        <div className="logosection">
           <img src={require("../Assets/logoonly.png")} alt="Logo" height="40" />
           <img src={require("../Assets/appname.png")} alt="name" height="40" />
         </div>
-        <form id="formsection">
+        <form id="loginform" onSubmit={handleSubmit}>
           <br />
           <h3>Sign in</h3>
-          <input type="text" id="email" name="email" placeholder=" Email" />
+          {/*  {errors? <div>{errors}</div>:null} */}
+          <input
+            type="text"
+            id="email"
+            name="email"
+            className="forminput"
+            value={formData.email}
+            onChange={handleChange}
+            placeholder=" Email"
+          />
           <br />
           <br />
           <input
-            type="text"
+            type="password"
             id="password"
             name="password"
+            className="forminput"
+            value={formData.password}
+            onChange={handleChange}
             placeholder=" Password"
           />
           <br />
           <br />
-          <input type="submit" id ="submit" value="Sign in"></input>
+          <input type="submit" className="submit" value="Sign in"></input>
           <h3>
             New to Virtual Care?
             <a id="signuplink" href="/SignUp">
--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -4,10 +4,107 @@ const SignUp = () => {
   return (
     <>
       <div id="signup">
-        <div class="logosection">
+        <div className="logosection">
           <img src={require("../Assets/logoonly.png")} alt="Logo" height="40" />
           <img src={require("../Assets/appname.png")} alt="name" height="40" />
         </div>
+        <form id="signupform">
+          <br />
+          <h3>Create profile</h3>
+          <label for="fname">First Name</label>
+          <br />
+          <input
+            type="text"
+            id="fname"
+            name="fname"
+            className="forminput"
+            placeholder="First Name*"
+          />
+          <br />
+          <label for="lname">Last Name</label>
+          <br />
+          <input
+            type="text"
+            id="lname"
+            name="lname"
+            className="forminput"
+            placeholder="Last Name*"
+          />
+          <br />
+          <label for="lname">Date of birth</label>
+          <br />
+          <input
+            type="text"
+            id="dob"
+            name="dob"
+            className="forminput"
+            placeholder="MM/DD/YYYY"
+          />
+          <br />
+          <label for="sex">Sex</label>
+          <br />
+          <select id="sex" name="sex" className="forminput">
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </select>
+          <br />
+          <label for="insurance">Current Insurance</label>
+          <br />
+          <select id="insurance" name="insurance" className="forminput">
+            <option value="united health">United Health</option>
+            <option value="kaiser foundation">Kaiser Foundation</option>
+            <option value="blue cross blue shield ">
+              Blue Cross Blue Shield
+            </option>
+            <option value="kaiser foundation">Kaiser Foundation</option>
+          </select>
+          <br />
+          <label for="mobile">Mobile</label>
+          <br />
+          <input
+            type="text"
+            id=""
+            name="mobile"
+            className="forminput"
+            placeholder="XXX-XXX-XXXX"
+          />
+          <br />
+          <label for="email">Email</label>
+          <br />
+          <input
+            type="text"
+            id=""
+            name="email"
+            className="forminput"
+            placeholder="example@gmail.com"
+          />
+          <br />
+          <label for="password">Password</label>
+          <br />
+          <input
+            type="text"
+            id=""
+            name="password"
+            className="forminput"
+            placeholder="*********"
+          />
+          <br />
+          <h5>
+            By continuing, I agree to the Privacy Policy and Terms of Use.{" "}
+          </h5>
+          <input
+            type="submit"
+            className="submit"
+            value="Create Account"
+          ></input>
+          <h3>
+            Already have an an account?
+            <a id="signuplink" href="/login">
+              {" "}
+              Sign in
+            </a>
+          </h3>
+        </form>
       </div>
     </>
   );
